455d8cd290c3484faad51461384726f40cfbc591
kidchig <94409171+kidchig@users.noreply.github.com>
2022-06-15 10:16:24 +0530

    Removed Inspect Tool

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,3 +6,21 @@ $(document).ready(function () {
         $("#myVideo").get(0).play();
     });
 });
+
+document.onkeydown = function(e) {
+    if(window.keyCode == 123) {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+       return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+       return false;
+    }
+}  
